ad6edd42b469de0b448c34e49aad0a8a0b89c39e
DocBiola <105199096+DocBiola@users.noreply.github.com>
2022-06-24 08:09:43 -0700

    Add files via upload

---
+++ life.js
@@ -0,0 +1,9 @@
+let firstName= 'Abdulkareem'
+console.log (firstName);
+let lastName= 'Abdulmuheez'
+console.log (lastName);
+let myheight= '167cm'
+console.log (myheight);
+let myCountry= 'Nigeria'
+console.log (myCountry);
+
